bc00f65f4f57cd728e687e7a5f16364333ae0a42
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-04-25 10:50:16 +0900

    [Silver III] Title: 바이러스, Time: 148 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,24 @@
+from collections import deque
+
 computer = int(input())
 connect = int(input())
 graph = [[] for _ in range(computer+1)]
-
 for _ in range(connect):
   x, y = map(int, input().split())
-  if y not in graph[x]:
-    graph[x].append(y)
-  if x not in graph[y]:
-    graph[y].append(x)
+  graph[x].append(y)
+  graph[y].append(x)
 
-visited = [False] * (computer + 1)
-count = 0
-def dfs(graph, v, visited):
-  global count
-  visited[v] = True
+count = -1
+visited = [False] * (computer+1)
+
+stack = deque([1])
+visited[1] = True
+while stack:
+  v = stack.pop()
   count += 1
   for i in graph[v]:
     if not visited[i]:
-      dfs(graph, i, visited)
-dfs(graph, 1, visited)
-print(count - 1)
+      stack.append(i)
+      visited[i] = True
+
+print(count)
