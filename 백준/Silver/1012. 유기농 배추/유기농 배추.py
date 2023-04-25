39f7c0aa5b89eb3feb0166c3d591125685ffc344
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-04-25 12:08:56 +0900

    [Silver II] Title: 유기농 배추, Time: 172 ms, Memory: 242300 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,28 +1,29 @@
 import sys
 sys.setrecursionlimit(100000)
 
-def dfs(x, y):
-  visited[x][y] = True
-  directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-  for dx, dy in directions:
-    nx, ny = x + dx, y + dy
-    if nx < 0 or nx >= N or ny < 0 or ny >= M:
-      continue
-    if field[nx][ny] and not visited[nx][ny]:
-      dfs(nx, ny)
-
+def dfs(y, x):
+  if x < 0 or x >= M or y < 0 or y >= N:
+    return False
+  if field[y][x] == 1:
+    field[y][x] = 0
+    dfs(y+1, x)
+    dfs(y, x+1)
+    dfs(y-1, x)
+    dfs(y, x-1)
+    return True
+  else:
+    return False
+    
 for _ in range(int(input())):
   M, N, K = map(int, input().split())
   field = [[0] * M for _ in range(N)]
-  visited = [[False] * M for _ in range(N)]
   for _ in range(K):
-    y, x = map(int, input().split())
-    field[x][y] = 1
+    x, y = map(int, input().split())
+    field[y][x] = 1
 
-  result = 0
-  for x in range(N):
-    for y in range(M):
-      if field[x][y] and not visited[x][y]:
-        dfs(x, y)
-        result += 1
-  print(result)
+  count = 0
+  for y in range(N):
+    for x in range(M):
+      if dfs(y, x):
+        count += 1
+  print(count)
