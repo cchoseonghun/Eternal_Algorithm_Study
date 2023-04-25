ebde188867ff35720de81ec76986c60932d3bf08
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-04-25 21:31:03 +0900

    [Gold IV] Title: N-Queen, Time: 29356 ms, Memory: 205044 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,21 +1,23 @@
-def is_available(candidate, curr_col):
-  curr_row = len(candidate)
-  for queen_row in range(curr_row):
-    if candidate[queen_row] == curr_col or abs(curr_col - candidate[queen_row]) == curr_row - queen_row:
+def is_available(x):
+  for i in range(x):
+    if row[x] == row[i]:
+      return False
+    if abs(row[x] - row[i]) == x - i:
       return False
   return True
 
-def dfs(N, candidate, result):
-  if len(candidate) == N:
-    result.append([*candidate])
-    return
-  for curr_col in range(N):
-    if is_available(candidate, curr_col):
-      candidate.append(curr_col)
-      dfs(N, candidate, result)
-      candidate.pop()
+def DFS(x):
+  global result
+  if x == N:
+    result += 1
+  else:
+    for i in range(N):
+      row[x] = i
+      if is_available(x):
+        DFS(x+1)
 
 N = int(input())
-result = []
-dfs(N, [], result)
-print(len(result))
+row = [0] * N
+result = 0
+DFS(0)
+print(result)
