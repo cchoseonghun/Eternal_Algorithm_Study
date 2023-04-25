f8acc2ec6361e0f6f9642d955730a8c9e0f73c62
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-04-25 17:54:04 +0900

    [Gold IV] Title: 해킹, Time: 1000 ms, Memory: 150480 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.py
+++ 백준/Gold/10282. 해킹/해킹.py
@@ -1,38 +1,37 @@
 import heapq
 
+def dijkstra(start):
+  distances[start] = 0
+  queue = []
+  heapq.heappush(queue, (0, start))
+
+  while queue:
+    curr_distance, curr_node = heapq.heappop(queue)
+
+    if distances[curr_node] < curr_distance:
+      continue
+
+    for n_node, n_distance in graph[curr_node]:
+      distance = curr_distance + n_distance
+      if distances[n_node] > distance:
+        distances[n_node] = distance
+        heapq.heappush(queue, (distance, n_node))
+
 for _ in range(int(input())):
-  n, d, c = map(int, input().split())
-  graph = { computer: {} for computer in range(1, n+1) }
+  n, d, start = map(int, input().split())
+  graph = [[] for _ in range(n+1)]
+  distances = [float('inf')] * (n+1)
+
   for _ in range(d):
     a, b, s = map(int, input().split())
-    graph[b][a] = s
+    graph[b].append((a, s))
 
-  def dijkstra(graph, start):
-    distances = { node: float('inf') for node in graph }
-    distances[start] = 0
-    queue = []
-    heapq.heappush(queue, [distances[start], start])
-
-    while queue:
-      curr_distance, curr_node = heapq.heappop(queue)
-
-      if distances[curr_node] < curr_distance:
-        continue
-
-      for n_node, n_distance in graph[curr_node].items():
-        distance = curr_distance + n_distance
-        if distance < distances[n_node]:
-          distances[n_node] = distance
-          heapq.heappush(queue, [distance, n_node])
-    
-    return distances
-
-  result = dijkstra(graph, c)
+  dijkstra(start)
 
   count = 0
   max_distance = 0
-  for i in result:
-    if result[i] != float('inf'):
+  for i in distances:
+    if i != float('inf'):
       count += 1
-      max_distance = max(max_distance, result[i])
+      max_distance = max(max_distance, i)
   print(count, max_distance)
