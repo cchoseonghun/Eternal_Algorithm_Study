87401a9a0b6709e1521cf2dd5955ad355eb26402
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-04-27 15:15:10 +0900

    [Bronze II] Title: 수 정렬하기, Time: 136 ms, Memory: 114984 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,16 +1,6 @@
-N = int(input())
 arr = []
-
-for _ in range(N):
+for _ in range(int(input())):
   arr.append(int(input()))
-
-def select_sort(arr):
-  for i in range(len(arr)):  # i -> 0, 1, 2, 3, 4
-    min_index = i
-    for j in range(i, len(arr)):
-      if arr[min_index] > arr[j]:
-        min_index = j
-    print(arr[min_index])
-    arr[i], arr[min_index] = arr[min_index], arr[i]
-
-select_sort(arr)
+arr.sort()
+for x in arr:
+  print(x)
