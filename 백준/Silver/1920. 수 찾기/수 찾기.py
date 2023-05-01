f6f83eba081e433e20287504cab4591c3a50745a
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-01 23:48:25 +0900

    [Silver IV] Title: 수 찾기, Time: 232 ms, Memory: 148880 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,23 +1,11 @@
 N = int(input())
-arr = list(map(int, input().split()))
-arr.sort()
+arr = set(map(int, input().split()))
 M = int(input())
 target_arr = list(map(int, input().split()))
 
 
-def search(arr, target):
-  start = 0
-  end = N-1
-  while start <= end:
-    mid = (start + end) // 2
-    if arr[mid] == target:
-      return 1
-    elif arr[mid] < target:
-      start = mid + 1
-    else:
-      end = mid - 1
-  return 0
-
-
-for x in target_arr:
-  print(search(arr, x))
+for target in target_arr:
+  if target not in arr:
+    print(0)
+  else:
+    print(1)
