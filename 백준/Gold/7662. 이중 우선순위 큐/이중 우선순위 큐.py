d021fb45dacbaced740faae6c8158671d1c687ec
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-07 15:40:49 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 3812 ms, Memory: 350112 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,45 +1,36 @@
-import heapq
 import sys
 input = sys.stdin.readline
+import heapq
+
+def pop(heap):
+  while len(heap) > 0:
+    data, id = heapq.heappop(heap)
+    if not deleted[id]: 
+      deleted[id] = True
+      return data
+  return None
 
 for _ in range(int(input())):
   K = int(input())
   minheap = []
   maxheap = []
-  deleted = []
-
   index = 0
+  deleted = [False] * (K + 1)
   for _ in range(K):
-    command, n = input().split()
-    if command == 'I':
-      heapq.heappush(minheap, (int(n), index))
-      heapq.heappush(maxheap, (-int(n), index))
-      deleted.append(False)
+    command = input().split()
+    operator, data = command[0], int(command[1])
+    if operator == 'D':
+      if data == -1:
+        pop(minheap)
+      elif data == 1:
+        pop(maxheap)
+    elif operator == 'I':
+      heapq.heappush(minheap, (data, index))
+      heapq.heappush(maxheap, (-data, index))
       index += 1
-    elif command == 'D':
-      if int(n) > 0:
-        while len(maxheap) > 0:
-          x, i = heapq.heappop(maxheap)
-          if not deleted[i]:
-            deleted[i] = True
-            break
-      if int(n) < 0:
-        while len(minheap) > 0:
-          x, i = heapq.heappop(minheap)
-          if not deleted[i]:
-            deleted[i] = True
-            break
-
-  if deleted.count(True) == len(deleted):
+  max_value = pop(maxheap)
+  if max_value == None:
     print('EMPTY')
   else:
-    while True:
-      x, i = heapq.heappop(maxheap)
-      if not deleted[i]:
-        print(-x, end=' ')
-        break
-    while True:
-      x, i = heapq.heappop(minheap)
-      if not deleted[i]:
-        print(x, end=' ')
-        break
+    heapq.heappush(minheap, (-max_value, index))
+    print(-max_value, pop(minheap))
