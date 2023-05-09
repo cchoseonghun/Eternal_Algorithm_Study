a5012771afc3f801931940343b4b6a134957c387
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-09 15:38:59 +0900

    [Gold V] Title: 강의실, Time: 576 ms, Memory: 130128 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.py
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -8,20 +8,18 @@ for _ in range(N):
   _, start, end = map(int, input().split())
   heapq.heappush(arr, (start, end))
 
-heap = []  # 배정된 강의실
+heap = []
 end = heapq.heappop(arr)[1]
 heapq.heappush(heap, end)
-answer = 1
 
-for i in range(N-1):
+while arr:
   new_start, new_end = heapq.heappop(arr)
   end = heapq.heappop(heap)
 
   if new_start < end:
     heapq.heappush(heap, end)
     heapq.heappush(heap, new_end)
-    answer += 1
   else:
     heapq.heappush(heap, new_end)
 
-print(answer)
+print(len(heap))
