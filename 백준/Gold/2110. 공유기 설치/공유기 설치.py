37d58fb2581754ebf2e1486e20fde1486a9a3308
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-10 18:02:03 +0900

    [Gold IV] Title: 공유기 설치, Time: 184 ms, Memory: 116556 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,28 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-def solution(arr, N, C):
-  start = 1
-  end = arr[-1] - arr[0]
-  result = 0
-
-  while start <= end:
-    mid = (start + end) // 2
-    prev = arr[0]
-    count = 1
-    for i in range(1, N):
-      if arr[i] >= prev + mid:
-        prev = arr[i]
-        count += 1
-    if count >= C:
-      result = mid
-      start = mid + 1
-    else:
-      end = mid - 1
-
-  return result
-
 N, C = map(int, input().split())
 arr = [int(input()) for _ in range(N)]
 arr.sort()
-print(solution(arr, N, C))
+
+start = 1
+end = arr[-1] - arr[0]
+result = 0
+
+while start <= end:
+  mid = (start + end) // 2
+  prev = arr[0]
+  count = 1
+  for x in arr:
+    if prev + mid <= x:
+      prev = x
+      count += 1
+  if count >= C:
+    start = mid + 1
+    result = mid
+  else:
+    end = mid - 1
+    
+print(result)
