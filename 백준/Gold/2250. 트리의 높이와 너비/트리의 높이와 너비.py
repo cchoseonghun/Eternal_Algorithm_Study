060b8e1ba558a98b57223bb4e5b9dda840393ec5
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-11 11:11:27 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 200 ms, Memory: 119592 KB -BaekjoonHub

--- 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.py
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.py
@@ -1,54 +1,54 @@
-class Node:
-  def __init__(self, number, left_node, right_node):
-    self.parent = -1
-    self.number = number
-    self.left_node = left_node
-    self.right_node = right_node
+import sys
+input = sys.stdin.readline
 
-def in_order(node, level):
-  global level_depth, x
-  level_depth = max(level_depth, level)
-  if node.left_node != -1:
-    in_order(tree[node.left_node], level + 1)
-  level_min[level] = min(level_min[level], x)
-  level_max[level] = max(level_max[level], x)
+class Node:
+  def __init__(self, data, left, right):
+    self.parent = -1
+    self.data = data
+    self.left = left
+    self.right = right
+
+def in_order(L, node):
+  global x
+  while len(arr) < L + 1:
+    arr.append([])
+  if node.left != -1:
+    in_order(L + 1, tree[node.left])
+  arr[L].append(x)
   x += 1
-  if node.right_node != -1:
-    in_order(tree[node.right_node], level + 1)
+  if node.right != -1:
+    in_order(L + 1, tree[node.right])
 
 N = int(input())
 tree = {}
-level_min = [N]
-level_max = [0]
-root = -1
-x = 1
-level_depth = 1
-
 for i in range(1, N + 1):
   tree[i] = Node(i, -1, -1)
-  level_min.append(N)
-  level_max.append(0)
 
 for _ in range(N):
-  number, left_node, right_node = map(int, input().split())
-  tree[number].left_node = left_node
-  tree[number].right_node = right_node
-  if left_node != -1:
-    tree[left_node].parent = number
-  if right_node != -1:
-    tree[right_node].parent = number
+  data, left, right = map(int, input().split())
+  tree[data].left = left
+  tree[data].right = right
+  if left != -1:
+    tree[left].parent = data
+  if right != -1:
+    tree[right].parent = data
 
+x = 1
+L = 1
+arr = [[]]
+root = 1
 for i in range(1, N + 1):
   if tree[i].parent == -1:
     root = i
+in_order(L, tree[root])
 
-in_order(tree[root], 1)
-result_level = 1
-result_width = level_max[1] - level_min[1] + 1
-for i in range(2, level_depth + 1):
-  width = level_max[i] - level_min[i] + 1
-  if result_width < width:
-    result_level = i
-    result_width = width
+max_i = 0
+max_w = 0
+for i in range(len(arr)):
+  if len(arr[i]) > 0:
+    new_w = max(arr[i]) - min(arr[i]) + 1
+    if max_w < new_w:
+      max_i = i
+      max_w = new_w
 
-print(result_level, result_width)
+print(max_i, max_w)
