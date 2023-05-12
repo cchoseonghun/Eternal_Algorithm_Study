9b9f18d0b6d94a1b27581ae1396a4c993ea95fc5
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-13 03:05:29 +0900

    [Silver I] Title: 기타리스트, Time: 124 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.py
+++ 백준/Silver/1495. 기타리스트/기타리스트.py
@@ -1,7 +1,7 @@
 N, S, M = map(int, input().split())
 arr = list(map(int, input().split()))
 
-dp = [[0] * (M + 1) for _ in range(N + 1)]
+dp = [[False] * (M + 1) for _ in range(N + 1)]
 dp[0][S] = True
 
 for i in range(1, N + 1):
