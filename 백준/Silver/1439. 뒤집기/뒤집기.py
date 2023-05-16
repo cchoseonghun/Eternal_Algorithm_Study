0cb639c9a19108c820da3e1002f4e75e3e079baf
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-16 19:14:04 +0900

    [Silver V] Title: 뒤집기, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,21 +1,16 @@
 S = input()
+to_one, to_zero = 0, 0
 
-def turn_zero(S):
-  count = 0
-  for i in range(len(S)-1):
-    if int(S[i]) - int(S[i+1]) == 1:
-      count += 1
-    if (int(S[-1]) == 1 and int(S[-2]) == 1) or (int(S[-1]) == 1 and int(S[-2]) == 0):
-      count += 1
-  return count
+if S[0] == '0':
+  to_one += 1
+else:
+  to_zero += 1
 
-def turn_one(S):
-  count = 0
-  for i in range(len(S)-1):
-    if int(S[i+1]) - int(S[i]) == 1:
-      count += 1
-    if (int(S[-1]) == 0 and int(S[-2]) == 0) or (int(S[-1]) == 0 and int(S[-2]) == 1):
-      count += 1
-  return count
+for i in range(len(S) - 1):
+  if S[i] != S[i + 1]:
+    if S[i + 1] == '1':
+      to_zero += 1
+    else:
+      to_one += 1
 
-print(min(turn_zero(S), turn_one(S)))
+print(min(to_one, to_zero))
