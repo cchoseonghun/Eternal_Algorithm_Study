ad0de2140ad0844a049c1358ef984695c4d44e7f
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-16 15:28:02 +0900

    [Gold IV] Title: 해킹, Time: 812 ms, Memory: 151456 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.py
+++ 백준/Gold/10282. 해킹/해킹.py
@@ -1,41 +1,35 @@
-import heapq
 from collections import defaultdict
+import heapq
 import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-  N, D, C = map(int, input().split())
+  n, d, c = map(int, input().split())
   graph = defaultdict(list)
-  d = {}
-  for _ in range(D):
+  distances = defaultdict(list)
+  for _ in range(d):
     a, b, s = map(int, input().split())
-    if b not in graph:
-      graph[b] = [(s, a)]
-    else:
-      graph[b].append((s, a))
-
-    if a not in d:
-      d[a] = 1e9
-    if b not in d:
-      d[b] = 1e9
-  d[C] = 0
+    graph[b].append((s, a))
+    distances[a] = 1e9
+    distances[b] = 1e9
+  distances[c] = 0
 
   queue = []
-  heapq.heappush(queue, (0, C))
+  heapq.heappush(queue, (0, c))
   while queue:
     curr_d, curr_n = heapq.heappop(queue)
-    if d[curr_n] < curr_d:
+    if distances[curr_n] < curr_d:
       continue
     for next_d, next_n in graph[curr_n]:
       new_d = curr_d + next_d
-      if new_d < d[next_n]:
-        d[next_n] = new_d
+      if new_d < distances[next_n]:
+        distances[next_n] = new_d
         heapq.heappush(queue, (new_d, next_n))
 
   count = 0
   m = 0
-  for _, distance in d.items():
-    if distance != 1e9:
+  for x in distances.values():
+    if x != 1e9:
       count += 1
-      m = max(m, distance)
+      m = max(m, x)
   print(count, m)
