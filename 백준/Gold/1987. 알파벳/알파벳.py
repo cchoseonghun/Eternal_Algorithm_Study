3ab760dfb0a5824c07651c98115ddc414fa50207
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-17 22:28:23 +0900

    [Gold IV] Title: 알파벳, Time: 7148 ms, Memory: 116816 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,24 +1,26 @@
-dy = [-1, 0, 1, 0]
-dx = [0, 1, 0, -1]
-
 def BFS(x, y):
   result = 0
-  candidate = set()
-  candidate.add((x, y, board[y][x]))
-  while candidate:
-    x, y, step = candidate.pop()
-    result = max(result, len(step))
-    
+  queue = set()
+  queue.add((x, y, board[y][x]))
+
+  while queue:
+    cx, cy, candidate = queue.pop()
+    result = max(result, len(candidate))
     for i in range(4):
-      ny = y + dy[i]
-      nx = x + dx[i]
-      if (0 <= ny < R and 0 <= nx < C and board[ny][nx] not in step):
-        candidate.add((nx, ny, step + board[ny][nx]))
+      ny = cy + dy[i]
+      nx = cx + dx[i]
+      if (0 <= ny < R and 0 <= nx < C and board[ny][nx] not in candidate):
+        queue.add((nx, ny, candidate + board[ny][nx]))
+
   return result
+
 
 R, C = map(int, input().split())
 board = []
 for _ in range(R):
   board.append(input())
+
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
 
 print(BFS(0, 0))
