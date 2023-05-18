bc2be8248c165bc173ccf378b33983a5cd64774c
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-18 21:27:03 +0900

    [Gold IV] Title: 알파벳, Time: 5636 ms, Memory: 149736 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,26 +1,25 @@
-def BFS(x, y):
-  result = 0
-  queue = set()
-  queue.add((x, y, board[y][x]))
+# 상, 하, 좌, 우 방향으로 이동하기 위한 dx, dy 값
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-  while queue:
-    cx, cy, candidate = queue.pop()
-    result = max(result, len(candidate))
+def dfs(x, y, cnt):
+    global answer
+    answer = max(answer, cnt)  # 최댓값 갱신
+    visited[ord(board[x][y]) - ord('A')] = True  # 방문 처리
+
     for i in range(4):
-      ny = cy + dy[i]
-      nx = cx + dx[i]
-      if (0 <= ny < R and 0 <= nx < C and board[ny][nx] not in candidate):
-        queue.add((nx, ny, candidate + board[ny][nx]))
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < R and 0 <= ny < C and not visited[ord(board[nx][ny]) - ord('A')]:
+            dfs(nx, ny, cnt + 1)  # 이동하면서 카운트 증가
 
-  return result
-
+    visited[ord(board[x][y]) - ord('A')] = False  # 백트래킹
 
 R, C = map(int, input().split())
-board = []
-for _ in range(R):
-  board.append(input())
+board = [list(input().rstrip()) for _ in range(R)]
+visited = [False] * 26  # 알파벳 방문 여부를 체크하기 위한 배열
+answer = 1  # 최대 칸의 수 초기화
 
-dy = [-1, 0, 1, 0]
-dx = [0, 1, 0, -1]
+dfs(0, 0, 1)  # 시작 지점부터 DFS 탐색 시작
 
-print(BFS(0, 0))
+print(answer)
