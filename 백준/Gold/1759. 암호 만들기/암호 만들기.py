fe5fc97701fbc699c7598d5f6598c00a29f4c720
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-19 01:02:20 +0900

    [Gold V] Title: 암호 만들기, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,25 +1,19 @@
-def nCr(level, start):
-  if level == L:
-    if check(result):
-      print(''.join(result))
-  else:
-    for i in range(start, len(arr)):
-      result[level] = arr[i]
-      nCr(level + 1, i + 1)
+from itertools import combinations
 
-def check(target):
+check = ['a', 'e', 'i', 'o', 'u']
+
+def is_available(target):
   count = 0
   for x in target:
-    if x in M:
+    if x in check:
       count += 1
-  if 1 <= count <= L - 2:
+  if 1 <= count <= R - 2:
     return True
-  else:
-    return False
+  return False
 
-L, C = map(int, input().split())
+R, C = map(int, input().split())
 arr = list(input().split())
 arr.sort()
-M = ['a', 'e', 'i', 'o', 'u']
-result = [0] * L
-nCr(0, 0)
+for x in combinations(arr, R):
+  if is_available(x):
+    print(''.join(x))
