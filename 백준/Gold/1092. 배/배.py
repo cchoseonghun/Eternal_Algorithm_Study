30c2a529b9ba5832cfbb4afd692dfceed10890a9
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-19 15:11:32 +0900

    [Gold V] Title: 배, Time: 124 ms, Memory: 115452 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -1,37 +1,28 @@
-import sys
-input = sys.stdin.readline
-
-
-
 N = int(input())
 cranes = list(map(int, input().split()))
-
 M = int(input())
 boxes = list(map(int, input().split()))
 
 if max(cranes) < max(boxes):
   print(-1)
-  sys.exit()
+  exit()
 
-positions = [0] * N
-checked = [False] * M
 cranes.sort(reverse=True)
 boxes.sort(reverse=True)
 
-result = 0
-count = 0
-
+positions = [0] * N  # 각 index 번째 크레인이 바라보는 boxes index
+checked = [False] * M  # boxes index의 박스가 옮겨졌는지 체크
+count = 0  # 옮긴 box 수
+result = 0  # 소요된 시간
 while True:
-  if count == len(boxes):
+  if count == M:
     break
   for i in range(N):
-    while positions[i] < len(boxes):
-      if not checked[positions[i]] and cranes[i] >= boxes[positions[i]]:
+    while positions[i] < M:
+      if boxes[positions[i]] <= cranes[i] and not checked[positions[i]]:
         checked[positions[i]] = True
-        positions[i] += 1
         count += 1
         break
       positions[i] += 1
   result += 1
-
 print(result)
