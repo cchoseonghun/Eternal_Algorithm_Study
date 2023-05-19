67a25ed9834c5517657f304378c15de12b3f917f
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-19 17:45:19 +0900

    [Silver I] Title: 기타리스트, Time: 120 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.py
+++ 백준/Silver/1495. 기타리스트/기타리스트.py
@@ -1,22 +1,21 @@
 N, S, M = map(int, input().split())
 arr = list(map(int, input().split()))
-
 dp = [[False] * (M + 1) for _ in range(N + 1)]
 dp[0][S] = True
 
 for i in range(1, N + 1):
   for j in range(M + 1):
-    if not dp[i - 1][j]:
-      continue
-    else:
-      if j - arr[i - 1] >= 0:
-        dp[i][j - arr[i - 1]] = True
-      if j + arr[i - 1] <= M:
-        dp[i][j + arr[i - 1]] = True
+    if dp[i - 1][j]:
+      a = j - arr[i - 1]
+      b = j + arr[i - 1]
+      if 0 <= a <= M:
+        dp[i][a] = True
+      if 0 <= b <= M:
+        dp[i][b] = True
 
 result = -1
 for i in range(M, -1, -1):
-  if dp[N][i]:
+  if dp[-1][i]:
     result = i
     break
 print(result)
