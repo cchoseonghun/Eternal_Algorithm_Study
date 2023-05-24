340f96bc5e50c48d597eaa432b03afb6b893a8e1
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-24 15:22:11 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 200 ms, Memory: 116104 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -4,20 +4,10 @@ input = sys.stdin.readline
 for _ in range(int(input())):
   N = int(input())
   arr = list(map(int, input().split()))
-  arr.sort(reverse=True)
-  C = N // 2
+  arr.sort()
 
-  result = [0] * N
-  result[C] = arr[0]
-
-  for i in range(1, N):
-    if i % 2 == 1:
-      result[C - i // 2 - 1] = arr[i]
-    else:
-      result[C + i // 2] = arr[i]
-  
-  M= 0
-  for i in range(N):
-    L = abs(result[i] - result[(i + 1) % N])
+  M = 0
+  for i in range(N - 2):
+    L = abs(arr[i] - arr[i + 2])
     M = max(M, L)
   print(M)
