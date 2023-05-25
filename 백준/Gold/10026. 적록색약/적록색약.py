1446ba9df8d6b279783433b9e5a3900756124f32
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-25 21:51:01 +0900

    [Gold V] Title: 적록색약, Time: 260 ms, Memory: 255624 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -13,30 +13,30 @@ def DFS(x, y, target):
     if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and board[nx][ny] == target:
       DFS(nx, ny, target)
 
+def DFS2(x, y, target):
+  visited2[x][y] = True
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if 0 <= nx < N and 0 <= ny < N and not visited2[nx][ny]:
+      if (target == 'R' or target == 'G') and (board[nx][ny] == 'R' or board[nx][ny] == 'G'):
+        DFS2(nx, ny, target)
+      elif target == 'B' and board[nx][ny] == target:
+        DFS2(nx, ny, target)
+
 N = int(input())
 board = [list(input()) for _ in range(N)]
 
 visited = [[False] * N for _ in range(N)]
+visited2 = [[False] * N for _ in range(N)]
 result = 0
+result2 = 0
 for i in range(N):
   for j in range(N):
     if not visited[i][j]:
       DFS(i, j, board[i][j])
       result += 1
-
-print(result, end=' ')
-
-for i in range(N):
-  for j in range(N):
-    if board[i][j] == 'G':
-      board[i][j] = 'R'
-
-visited = [[False] * N for _ in range(N)]
-result = 0
-for i in range(N):
-  for j in range(N):
-    if not visited[i][j]:
-      DFS(i, j, board[i][j])
-      result += 1
-
-print(result, end=' ')
+    if not visited2[i][j]:
+      DFS2(i, j, board[i][j])
+      result2 += 1
+print(result, result2)
