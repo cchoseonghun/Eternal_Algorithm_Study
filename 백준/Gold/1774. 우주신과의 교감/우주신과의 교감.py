f8f05f78a5759caf06d7713c4313b826bd267541
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-27 18:34:15 +0900

    [Gold III] Title: 우주신과의 교감, Time: 1140 ms, Memory: 195180 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.py
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.py
@@ -4,27 +4,22 @@ input = sys.stdin.readline
 def get_distance(a, b):
   return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
 
-def get_parent(parent, n):
+def find(n):
   if parent[n] != n:
-    parent[n] = get_parent(parent, parent[n])
+    parent[n] = find(parent[n])
   return parent[n]
 
-def union(parent, a, b):
-  a = get_parent(parent, a)
-  b = get_parent(parent, b)
-  if a < b:
-    parent[b] = a
-  else:
-    parent[a] = b
-
-def find(parent, a, b):
-  a = get_parent(parent, a)
-  b = get_parent(parent, b)
-  if a == b:
+def union(a, b):
+  a = find(a)
+  b = find(b)
+  if a != b:
+    if a < b:
+      parent[b] = a
+    else:
+      parent[a] = b
     return True
-  else:
-    return False
-  
+  return False
+
 edges = []
 parent = {}
 locations = []
@@ -45,14 +40,13 @@ for i in range(1, N + 1):
 
 for i in range(M):
   a, b = map(int, input().split())
-  union(parent, a, b)
+  union(a, b)
 
 edges.sort(key=lambda data: data[2])
 
 result = 0
 for a, b, cost in edges:
-  if not find(parent, a, b):
-    union(parent, a, b)
+  if union(a, b):
     result += cost
 
 print('%0.2f'%result)
