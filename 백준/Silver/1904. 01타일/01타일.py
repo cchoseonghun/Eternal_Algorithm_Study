e917ddc395b3bd4a2feb7c60803e8a683edcb164
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-27 23:57:04 +0900

    [Silver III] Title: 01타일, Time: 124 ms, Memory: 122144 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -2,6 +2,6 @@ N = int(input())
 dp = [0] * 1000001
 dp[1] = 1
 dp[2] = 2
-for i in range(3, N+1):
-  dp[i] = (dp[i-2]+dp[i-1]) % 15746
+for i in range(3, N + 1):
+  dp[i] = (dp[i - 2] + dp[i - 1]) % 15746
 print(dp[N])
