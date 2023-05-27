f84e46cc3548a5048873c5554a9ade526d4d79a9
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-05-27 17:12:46 +0900

    [Gold IV] Title: 해킹, Time: 700 ms, Memory: 147016 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.py
+++ 백준/Gold/10282. 해킹/해킹.py
@@ -1,35 +1,32 @@
-from collections import defaultdict
 import heapq
 import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-  n, d, c = map(int, input().split())
-  graph = defaultdict(list)
-  distances = defaultdict(list)
-  for _ in range(d):
+  N, D, C = map(int, input().split())
+  graph = [[] for _ in range(N + 1)]
+  distances = [1e9] * (N + 1)
+  distances[C] = 0
+  for _ in range(D):
     a, b, s = map(int, input().split())
     graph[b].append((s, a))
-    distances[a] = 1e9
-    distances[b] = 1e9
-  distances[c] = 0
 
-  queue = []
-  heapq.heappush(queue, (0, c))
-  while queue:
-    curr_d, curr_n = heapq.heappop(queue)
+  heap = []
+  heapq.heappush(heap, (0, C))
+  while heap:
+    curr_d, curr_n = heapq.heappop(heap)
     if distances[curr_n] < curr_d:
       continue
     for next_d, next_n in graph[curr_n]:
       new_d = curr_d + next_d
       if new_d < distances[next_n]:
         distances[next_n] = new_d
-        heapq.heappush(queue, (new_d, next_n))
+        heapq.heappush(heap, (new_d, next_n))
 
   count = 0
-  m = 0
-  for x in distances.values():
+  timer = 0
+  for x in distances:
     if x != 1e9:
       count += 1
-      m = max(m, x)
-  print(count, m)
+      timer = max(timer, x)
+  print(count, timer)
