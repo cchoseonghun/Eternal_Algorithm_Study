59b6a45ef5e6c33941d2605e867ef107303d23de
조성훈 <109499274+cchoseonghun@users.noreply.github.com>
2023-07-02 00:42:14 +0900

    [Silver I] Title: 트리 순회, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,38 +1,46 @@
+import sys
+input = sys.stdin.readline
+
 class Node:
-  def __init__(self, data, left, right):
+  def __init__(self, data):
     self.data = data
-    self.left = left
-    self.right = right
+    self.left = None
+    self.right = None
 
 def pre_order(node):
   print(node.data, end='')
-  if node.left != '.':
-    pre_order(tree[node.left])
-  if node.right != '.':
-    pre_order(tree[node.right])
-
+  if node.left != None:
+    pre_order(nodes[node.left])
+  if node.right != None:
+    pre_order(nodes[node.right])
+  
 def in_order(node):
-  if node.left != '.':
-    in_order(tree[node.left])
+  if node.left != None:
+    in_order(nodes[node.left])
   print(node.data, end='')
-  if node.right != '.':
-    in_order(tree[node.right])
+  if node.right != None:
+    in_order(nodes[node.right])
 
 def post_order(node):
-  if node.left != '.':
-    post_order(tree[node.left])
-  if node.right != '.':
-    post_order(tree[node.right])
+  if node.left != None:
+    post_order(nodes[node.left])
+  if node.right != None:
+    post_order(nodes[node.right])
   print(node.data, end='')
 
 N = int(input())
-tree = {}
+nodes = {}
 for _ in range(N):
   data, left, right = input().split()
-  tree[data] = Node(data, left, right)
-
-pre_order(tree['A'])
+  node = Node(data)
+  if left != '.':
+    node.left = left
+  if right != '.':
+    node.right = right
+  nodes[data] = node
+  
+pre_order(nodes['A'])
 print()
-in_order(tree['A'])
+in_order(nodes['A'])
 print()
-post_order(tree['A'])
+post_order(nodes['A'])
